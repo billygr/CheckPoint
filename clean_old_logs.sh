6f71d3878a9ebe4d2d71ec58c8b573988ae15bc9
billy <billygr@users.noreply.github.com>
2019-12-04 12:18:12 +0200

    Ignore files not found

--- clean_old_logs.sh
+++ clean_old_logs.sh
@@ -2,31 +2,31 @@
 # Clean old logfiles by billy
 # R80.30 based no confirmation
 
-rm /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/*
-rm /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/Other/*
-rm /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/Policy_Installation/*
-rm /var/log/opt/CPsuite-R80.30/fw1/log/sduu-2019*
+rm -f /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/*
+rm -f /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/Other/*
+rm -f /var/log/opt/CPsuite-R80.30/fw1/log/failed_tasks/Policy_Installation/*
+rm -f /var/log/opt/CPsuite-R80.30/fw1/log/sduu-2019*
 
 # SOLR / CPSEAD / SMARTVIEW
-rm /var/log/opt/CPrt-R80.30/log/solr.log.??
-rm /var/log/opt/CPrt-R80.30/log/solr.log.?
-rm /var/log/opt/CPrt-R80.30/log/cpsead.elg.?
-rm /var/log/opt/CPrt-R80.30/log/smartview-service.log.?
-rm /var/log/opt/CPrt-R80.30/log/smartview-service.log.??
+rm -f /var/log/opt/CPrt-R80.30/log/solr.log.??
+rm -f /var/log/opt/CPrt-R80.30/log/solr.log.?
+rm -f /var/log/opt/CPrt-R80.30/log/cpsead.elg.?
+rm -f  /var/log/opt/CPrt-R80.30/log/smartview-service.log.?
+rm -f /var/log/opt/CPrt-R80.30/log/smartview-service.log.??
 
 # LOG INDEXER
-rm /var/log/opt/CPrt-R80.30/log_indexer/log/log_indexer.elg.?
+rm -f /var/log/opt/CPrt-R80.30/log_indexer/log/log_indexer.elg.?
 
 # CPM
-rm /var/log/opt/CPsuite-R80.30/fw1/log/cpm.elg.??
-rm /var/log/opt/CPsuite-R80.30/fw1/log/cpm.elg.?
+rm -f  /var/log/opt/CPsuite-R80.30/fw1/log/cpm.elg.??
+rm -f  /var/log/opt/CPsuite-R80.30/fw1/log/cpm.elg.?
 
 # FWD
-rm /var/log/opt/CPsuite-R80.30/fw1/log/fwd.elg.?
+rm -f  /var/log/opt/CPsuite-R80.30/fw1/log/fwd.elg.?
 
 # cores
-rm /var/log/dump/usermode/*
-rm /home/admin/last_dump.log
+rm -f  /var/log/dump/usermode/*
+rm -f /home/admin/last_dump.log
 
 # Delete indexes after 30 days
 find /var/log/opt/CPrt-R80.30/log_indexes/other* -type d -ctime +30 -exec rm -rf {} \;
