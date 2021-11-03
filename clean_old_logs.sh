baf29219c8c03562681f3e00f07a2a4f4bdc0e79
billy <billygr@users.noreply.github.com>
2021-11-03 10:12:48 +0200

    Update clean_old_logs.sh

--- clean_old_logs.sh
+++ clean_old_logs.sh
@@ -8,8 +8,11 @@ rm -f $FWDIR/log/failed_tasks/*
 rm -f $FWDIR/log/failed_tasks/Other/*
 rm -f $FWDIR/log/failed_tasks/Policy_Installation/*
 rm -f $FWDIR/log/sduu-2019*
+rm -f $FWDIR/log/sduu-2020*
 rm -f $FWDIR/log/te_file_downloader.elg.?
 rm -f $FWDIR/log/api.elg.?
+rm -f $FWDIR/log/lpd.elg.?
+
 # R81
 rm -f $FWDIR/log/vmware-vmsvc.?.log
 rm -f $FWDIR/log/gaia_api_server.log.?
@@ -20,6 +23,8 @@ rm -f $RTDIR/log/solr.log.?
 rm -f $RTDIR/log/cpsead.elg.?
 rm -f $RTDIR/log/smartview-service.log.?
 rm -f $RTDIR/log/smartview-service.log.??
+rm -f $RTDIR/log/run_maintenance.elg.?
+rm -f $RTDIR/log/rfl_maintenance.elg.?
 
 # LOG INDEXER
 rm -f $RTDIR/log_indexer/log/log_indexer.elg.?
@@ -39,6 +44,8 @@ rm -f /home/admin/last_dump.log
 find $RTDIR/log_indexes/other* -type d -ctime +30 -exec rm -rf {} \;
 find $RTDIR/log_indexes/audit* -type d -ctime +30 -exec rm -rf {} \;
 find $RTDIR/log_indexes/smartevent* -type d -ctime +30 -exec rm -rf {} \;
+
+rm /var/log/gaia_api_server.log.?
 
 # messages/wtmp just a small gain 1MB
 #rm /var/log/messages.?
